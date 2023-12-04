748644dbb13c820ab3ba83759473810b733cd3da
FelipeGibinAntunes <felipegibinantunes@gmail.com>
2023-12-04 13:33:44 -0300

    #9

--- web/themes/custom/cuppies_theme/tests/elements.php
+++ web/themes/custom/cuppies_theme/tests/elements.php
@@ -3,7 +3,7 @@
 use Drupal\Tests\UnitTestCase;
 
 /**
- * Tests the removeClassName Drupal behavior.
+ * Tests the elements.js behavior.
  *
  * @group mymodule
  */
@@ -13,43 +13,37 @@ class elements extends UnitTestCase {
    * Test removeClassName behavior.
    */
   public function testRemoveClassNameBehavior() {
-    // Create a new Drupal context.
     $context = $this->getMockBuilder('Drupal\Core\Render\Placeholder\Context')
       ->disableOriginalConstructor()
       ->getMock();
 
-    // Replace this with your actual file path.
     $file_path = 'web\themes\custom\cuppies_theme\js\elements.js';
     require_once $file_path;
 
-    // Create an instance of the behavior.
     $behavior = new \Drupal\cuppies_theme\Plugin\cuppies_theme\behaviors\removeClassName();
 
-    // Replace this with your actual selector and class name.
     $selector = '.login-popup-form';
     $class_to_remove = 'disabled';
 
-    // Create a new DOM element for testing.
     $element = $this->getMockBuilder('DOMElement')
       ->disableOriginalConstructor()
       ->getMock();
 
-    // Set up the element to have the class you want to remove.
     $element->expects($this->once())
       ->method('getAttribute')
       ->with('class')
       ->willReturn($class_to_remove);
 
-    // Attach the behavior to the element.
     $behavior->attach($element, $context);
 
-    // Assert that the class has been removed.
     $this->assertEquals('', $element->getAttribute('class'));
   }
 
+   /**
+   * Test changeQuantity behavior.
+   */
   public function testChangeQuantityBehavior()
   {
-      // Mock the dependencies (e.g., jQuery, Drupal, drupalSettings)
       $jQueryMock = $this->getMockBuilder('jQuery')
           ->setMethods(['on', 'closest', 'find', 'siblings'])
           ->getMock();
@@ -83,25 +77,135 @@ class elements extends UnitTestCase {
        $this->assertNotEquals($oldQuantity, $changeQuantity->quantity);
   }
 
+   /**
+   * Test updateOnChange behavior.
+   */
+  public function testUpdateOnChangeBehavior () {
+    $updateOnChange = new \Drupal\behaviors\UpdateOnChange();
+    $updateOnChange->attach($contextMock, $settingsMock, $jQueryMock);
+    $oldQuantity = $currentQuantity->quantity;
+    $currentQuantity = $updateOnChange->getCurrentQuantity();
+    $totalSum = $updateOnChange->getTotalSum();
 
+    $contextMock = $this->getMockBuilder('Drupal\Core\Render\Element\RenderElement')
+        ->disableOriginalConstructor()
+        ->getMock();
 
+    $settingsMock = $this->getMockBuilder('Drupal\Core\Render\Element\RenderElement')
+        ->disableOriginalConstructor()
+        ->getMock();
 
+    $jQueryMock = $this->getMockBuilder('jQuery')
+        ->setMethods(['on', 'closest', 'find', 'siblings', 'attr', 'text', 'each'])
+        ->getMock();
 
+    $jQueryMock->expects($this->once())
+        ->method('on')
+        ->with('change', $this->isType('callable'));
 
+    $jQueryMock->expects($this->any())
+        ->method('each')
+        ->willReturnCallback(function ($callback) {
+            $callback($this->getMockBuilder('jQuery')->getMock());
+        });
 
+    $updateOnChange = new \Drupal\behaviors\UpdateOnChange();
+    $updateOnChange->attach($contextMock, $settingsMock, $jQueryMock);
 
+    $this->assertNotEquals($oldQuantity, $currentQuantity->quantity);
+}
 
+ /**
+ * Test showOverlay behavior.
+ */
+public function testShowOverlay() {
+  $contextMock = $this->getMockBuilder('Drupal\Core\Render\Element\RenderElement')
+  ->disableOriginalConstructor()
+  ->getMock();
 
+$settingsMock = $this->getMockBuilder('Drupal\Core\Render\Element\RenderElement')
+  ->disableOriginalConstructor()
+  ->getMock();
 
+  $jQueryMock = $this->getMockBuilder('jQuery')
+      ->setMethods(['on', 'show'])
+      ->getMock();
 
+  $jQueryMock->expects($this->once())
+      ->method('on')
+      ->with('click', $this->isType('callable'));
 
+  $jQueryMock->expects($this->once())
+      ->method('show');
 
+  $showOverlay = new \Drupal\behaviors\ShowOverlay();
+  $showOverlay->attach($contextMock, $settingsMock, $jQueryMock);
 
+  $overlayVisible = $showOverlay->isOverlayVisible();
 
+  $this->assertTrue($overlayVisible); 
+}
 
+ /**
+ * Test hideOverlay behavior.
+ */
+public function testAttachMethod() {
 
+  $contextMock = $this->getMockBuilder('Drupal\Core\Render\Element\RenderElement')
+  ->disableOriginalConstructor()
+  ->getMock();
 
+$settingsMock = $this->getMockBuilder('Drupal\Core\Render\Element\RenderElement')
+  ->disableOriginalConstructor()
+  ->getMock();
 
+  $jQueryMock = $this->getMockBuilder('jQuery')
+      ->setMethods(['on', 'hide'])
+      ->getMock();
 
-  
+  $jQueryMock->expects($this->exactly(2))
+      ->method('on')
+      ->withConsecutive(['click', $this->isType('callable')], ['click', $this->isType('callable')]);
+
+  $jQueryMock->expects($this->exactly(2))
+      ->method('hide');
+
+  $hideOverlay = new \Drupal\behaviors\HideOverlay();
+  $hideOverlay->attach($contextMock, $settingsMock, $jQueryMock);
+
+  $overlayHidden = $hideOverlay->isOverlayHidden();
+
+  $this->assertTrue($overlayHidden); 
+}
+
+ /**
+ * Test hideMessage behavior.
+ */
+public function testhideMessageBehavior() {
+  $contextMock = $this->getMockBuilder('Drupal\Core\Render\Element\RenderElement')
+  ->disableOriginalConstructor()
+  ->getMock();
+
+$settingsMock = $this->getMockBuilder('Drupal\Core\Render\Element\RenderElement')
+  ->disableOriginalConstructor()
+  ->getMock();
+
+  $jQueryMock = $this->getMockBuilder('jQuery')
+      ->setMethods(['on', 'hide'])
+      ->getMock();
+
+  $jQueryMock->expects($this->once())
+      ->method('on')
+      ->with('click', $this->isType('callable'));
+
+  $jQueryMock->expects($this->once())
+      ->method('hide');
+
+  $hideMessage = new \Drupal\behaviors\HideMessage();
+  $hideMessage->attach($contextMock, $settingsMock, $jQueryMock);
+
+  $messagesHidden = $hideMessage->areMessagesHidden();
+
+  $this->assertTrue($messagesHidden); 
+}
 }
